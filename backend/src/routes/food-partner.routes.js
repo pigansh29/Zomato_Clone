ee6d66472811ac8bc0c4290d4ece5c3f047cabec
Pigansh <gaurpigansh@gmail.com>
2025-12-26 17:56:54 +0530

    update food-partner.routes.js

--- backend/src/routes/food-partner.routes.js
+++ backend/src/routes/food-partner.routes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 /* /api/food-partner/:id */
 router.get("/:id",
-    authMiddleware.authUserMiddleware,
     foodPartnerController.getFoodPartnerById)
 
 module.exports = router;
